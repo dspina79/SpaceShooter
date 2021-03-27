1b0b1f87560a2bf64ce7c7d216dbeb01dc740d13
Dave Spina <dspina79@gmail.com>
2021-03-26 20:57:32 -0400

    Project initialization and cleanup. Creation of starfield

---
+++ SpaceShooter/SpaceShooter/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  SpaceShooter
+//
+//  Created by Dave Spina on 3/26/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+}
