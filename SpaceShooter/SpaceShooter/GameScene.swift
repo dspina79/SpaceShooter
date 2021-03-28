ba0ccc939bf128cea88564b53a929c6b4dc74b7d
Dave Spina <dspina79@gmail.com>
2021-03-27 21:34:46 -0400

    Added player and physics

--- SpaceShooter/SpaceShooter/GameScene.swift
+++ SpaceShooter/SpaceShooter/GameScene.swift
@@ -7,10 +7,34 @@
 
 import SpriteKit
 
+enum CollisionType: UInt32 {
+    case player = 1
+    case playerWewapon = 2
+    case enemy = 4
+    case enemyWeapon = 8
+}
+
 class GameScene: SKScene {
-    
+    let player = SKSpriteNode(imageNamed: "player")
     
     override func didMove(to view: SKView) {
+        if let particles = SKEmitterNode(fileNamed: "Starfield") {
+            particles.position = CGPoint(x: 1080, y: 0)
+            particles.advanceSimulationTime(60)
+            particles.zPosition = -1;
+            addChild(particles)
+        }
+        
+        player.name = "Player";
+        player.position.x = frame.minX + 75;
+        player.zPosition = 1;
+        addChild(player)
+    
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.texture!.size())
+        player.physicsBody?.categoryBitMask = CollisionType.player.rawValue
+        player.physicsBody?.collisionBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionType.enemy.rawValue | CollisionType.enemyWeapon.rawValue
+        player.physicsBody?.isDynamic = false
         
     }
     
